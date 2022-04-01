045c6155a8410a5a85bb56b1d572bdd4078a002c
nandoslayer <92729509+nandoslayer@users.noreply.github.com>
2022-04-01 18:57:29 -0300

    Update Painelweb.sh

--- Painelweb.sh
+++ Painelweb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 barra="\033[0m\e[34m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-[[ ! -e /bin/ipw ]] && echo "/root/Painelweb.sh" > /bin/ipw && chmod +x /bin/ipw #ACCESO RAPIDO
+[[ ! -e /bin/ipw ]] && echo "/root/Panelweb.sh" > /bin/ipw && chmod +x /bin/ipw #ACCESO RAPIDO
 fun_bar () {
 comando[0]="$1"
 comando[1]="$2"
@@ -32,8 +32,45 @@ x="ok"
 menu ()
 {
 #PAINEL A INSTALAR
+panel_v10 () {
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v10/Painel.sh > /dev/null 2>&1; chmod +x Painel.sh; ./Painel.sh
+}
+panel_v10_2 () {
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v10_2/install.sh > /dev/null 2>&1; chmod +x install.sh; ./install.sh
+}
+panel_v11 () {
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v11/Painelv11.sh > /dev/null 2>&1; chmod +x Painelv11.sh; ./Painelv11.sh
+}
+panel_v11_2 () {
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v11_2/install > /dev/null 2>&1; chmod +x install; ./install
+}
+panel_v12 () {
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v12/install > /dev/null 2>&1; chmod +x install; ./install
+}
+panel_v15 () {
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v15/install > /dev/null 2>&1; chmod +x install; ./install
+}
+panel_v15_2 () {
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v15_2/ocspanel > /dev/null 2>&1; chmod +x ocspanel; ./ocspanel
+}
+panel_v20 () {
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v20/install > /dev/null 2>&1; chmod 777 install* && ./install*
+}
+panel_v20_mod () {
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v20_mod/install > /dev/null 2>&1; chmod +x install; ./install
+}
+panel_v23 () {
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v23/install > /dev/null 2>&1; chmod +x install; ./install
+}
+panel_v23_2 () {
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v23_2/install > /dev/null 2>&1; chmod +x install; ./install
+}
 panel_v25 () {
-wget https://raw.githubusercontent.com/nandoslayer/plusnssh/ntech/painelVXX/install  > /dev/null 2>&1; chmod +x install; ./install
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v25/install > /dev/null 2>&1; chmod +x install; ./install
+}
+#UPDATE VIP-VPS v23 a v25
+panel_update2325 () {
+wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/panel_v23_2/atu-v23-p-v25 > /dev/null 2>&1; chmod +x atu-v23-p-v25; ./atu-v23-p-v25
 }
 #CLEAN FOLDER
 clean_folder () {
@@ -53,7 +90,7 @@ rm -rf $HOME/sshplus.sql*
 rm -rf $HOME/bd-v15.sql*
 rm -rf $HOME/ssh.sql*
 rm -rf $HOME/plus.sql*
-rm -rf $HOME/Panelweb.sh* > /dev/null 2>&1; wget https://raw.githubusercontent.com/nandoslayer/plusnssh/ntech/painelVXX/Panelweb.sh > /dev/null 2>&1
+rm -rf $HOME/Panelweb.sh* > /dev/null 2>&1; wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/Panelweb.sh > /dev/null 2>&1
 echo ""
 echo -e "\033[1;33m CLEAN FOLDER COM SUCESSO -\033[1;32m OK !\033[1;37m"
 sleep 4s
@@ -87,7 +124,7 @@ fun_bar "apt-get upgrade -y"
 fun_att () {
     service ssh restart > /dev/null 2>&1
     rm -rf $HOME/Panelweb.sh* > /dev/null 2>&1
-    wget https://raw.githubusercontent.com/nandoslayer/plusnssh/ntech/painelVXX/Panelweb.sh > /dev/null 2>&1
+    wget https://raw.githubusercontent.com/AAAAAEXQOSyIpN2JZ0ehUQ/SSHPLUS-MANAGER-FREE/master/Install/Panel_Web/Panelweb.sh > /dev/null 2>&1
 }
 fun_bar 'fun_att'
 echo ""
@@ -108,7 +145,19 @@ echo -e "$barra"
 echo -e "\E[41;1;37mINSTALL PAINEL REVENDA SSH       \033[1;32m[\033[1;37m  VERSAO: r002 \033[1;32m]\E[0m"
 echo -e "$barra"
 echo ""
-echo -e "\033[1;31m[\033[1;36m12\033[1;31m] \033[1;37m• \033[1;33mPAINEL VIP-VPS SSH V.XX      \033[1;32m(ADE) "
+echo -e "\033[1;31m[\033[1;36m01\033[1;31m] \033[1;37m• \033[1;33mPAINEL REVENDA SSH V10       \033[1;31m(ANT) " 
+echo -e "\033[1;31m[\033[1;36m02\033[1;31m] \033[1;37m• \033[1;33mPAINEL REVENDA SSH V10 2     \033[1;31m(DAN) " 
+echo -e "\033[1;31m[\033[1;36m03\033[1;31m] \033[1;37m• \033[1;33mPAINEL REVENDA SSH V11       \033[1;31m(ANT) " 
+echo -e "\033[1;31m[\033[1;36m04\033[1;31m] \033[1;37m• \033[1;33mPAINEL REVENDA SSH V11 2     \033[1;32m(NEW) " 
+echo -e "\033[1;31m[\033[1;36m05\033[1;31m] \033[1;37m• \033[1;33mPAINEL REVENDA SSH V12       \033[1;32m(NEW) " 
+echo -e "\033[1;31m[\033[1;36m06\033[1;31m] \033[1;37m• \033[1;33mPAINEL REVENDA SSH V15       \033[1;31m(ANT) " 
+echo -e "\033[1;31m[\033[1;36m07\033[1;31m] \033[1;37m• \033[1;33mPAINEL REVENDA SSH V15 2     \033[1;31m(OCS) "
+echo -e "\033[1;31m[\033[1;36m08\033[1;31m] \033[1;37m• \033[1;33mPAINEL REVENDA SSH V20       \033[1;32m(NEW) \033[1;37m∆ "
+echo -e "\033[1;31m[\033[1;36m09\033[1;31m] \033[1;37m• \033[1;33mPAINEL REVENDA SSH V20 MOD   \033[1;32m(NEW) " 
+echo -e "\033[1;31m[\033[1;36m10\033[1;31m] \033[1;37m• \033[1;33mPAINEL REVENDA SSH V23       \033[1;32m(NEW) " 
+echo -e "\033[1;31m[\033[1;36m11\033[1;31m] \033[1;37m• \033[1;33mPAINEL VIP-VPS SSH V.23      \033[1;32m(ADE) "
+echo -e "\033[1;31m[\033[1;36m12\033[1;31m] \033[1;37m• \033[1;33mPAINEL VIP-VPS SSH V.25      \033[1;32m(ADE) "
+echo -e "\033[1;31m[\033[1;36m13\033[1;31m] \033[1;37m• \033[1;33mUPDATE VIP-VPS V.23 PARA V25 \033[1;32m(ADE) \033[1;37m√"
 echo -e "\033[1;31m[\033[1;36m14\033[1;31m] \033[1;37m• \033[1;33mCLEAN FOLDER                 \033[1;36m(\033[1;31mINESTABLE\033[1;36m) \033[1;37m• "
 echo -e "\033[1;31m[\033[1;36m15\033[1;31m] \033[1;37m• \033[1;33mPAINEL REMOVE                \033[1;36m(\033[1;31mINESTABLE\033[1;36m) \033[1;37m• "
 echo -e "\033[1;31m[\033[1;36m16\033[1;31m] \033[1;35m[!] \033[1;32mACTUALIZAR                \033[1;31mRam:\033[1;37m $_usor "
@@ -120,27 +169,87 @@ echo ""
 echo -ne "\033[1;32mOQUE DESEJA FAZER \033[1;33m?\033[1;31m?\033[1;37m : "; read x
 
 case "$x" in 
-1)
+1 | 01)
+clear
+panel_v10
+exit;
+;;
+2 | 02)
+clear
+panel_v10_2
+exit;
+;;
+3 | 03)
+clear
+panel_v11
+exit;
+;;
+4 | 04)
+clear
+panel_v11_2
+exit;
+;;      
+5 | 05)
+clear
+panel_v12
+exit;
+;;
+6 | 06)
+clear
+panel_v15
+exit;
+;; 
+7 | 07)
+clear
+panel_v15_2
+exit;
+;;
+8 | 08)
+clear
+panel_v20
+exit;
+;;     
+9 | 09)
+clear
+panel_v20_mod
+exit;
+;;
+10)
+clear
+panel_v23
+exit;
+;;
+11)
+clear
+panel_v23_2
+exit;
+;;
+12)
 clear
 panel_v25
-exit;   
+exit;
+;;     
+13)
+clear
+panel_update2325
+exit;
 ;;
-2)
+14)
 clear
 clean_folder
 exit;
 ;;
-3)
+15)
 clear
 remove_panel
 exit;
 ;;
-4)
+16)
 clear
 atualizar
 exit;
 ;;
-5)
+17)
 clear
 remove_multiscripts
 exit;
