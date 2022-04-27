bc8c374ebce2177f28cb91a766060e1ca9b7eac9
nandoslayer <92729509+nandoslayer@users.noreply.github.com>
2022-04-27 20:41:27 -0300

    Update autobackup.sh

--- gestorssh/autobackup.sh
+++ gestorssh/autobackup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 clear
-rm /root/sshplus.sql > /dev/null 2>&1
+rm /root/backupsql/sshplus.sql > /dev/null 2>&1
 senha=$(cat /var/www/html/pages/system/pass.php |cut -d"'" -f2)
-mysqldump -u root -p$senha sshplus > /root/sshplus.sql
+mysqldump -u root -p$senha sshplus > /root/backupsql/sshplus.sql
