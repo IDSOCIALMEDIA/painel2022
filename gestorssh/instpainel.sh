ca996abeb3d32e118847cbcac0b8e7577abfd2fb
nandoslayer <92729509+nandoslayer@users.noreply.github.com>
2022-05-01 20:17:04 -0300

    Add files via upload

--- gestorssh/instpainel.sh
+++ gestorssh/instpainel.sh
@@ -8,21 +8,25 @@ echo -e "\E[44;1;37m    INSTALAR PAINELWEB GESTOR-SSH     \E[0m"
 echo ""
 echo -e "                 \033[1;31mBy @nandoslayer\033[1;36m"
 echo ""
-echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read
+echo -ne "\n\033[1;32mDE UM ENTER PARA \033[1;33mCONTINUAR...\033[1;37m: "; read -r
 clear
 echo -e "\n\033[1;36mINICIANDO INSTALAÇÃO \033[1;33mAGUARDE..."
 apt-get update -y > /dev/null 2>&1
 apt-get install figlet -y > /dev/null 2>&1
 rm /bin/pweb > /dev/null 2>&1
 sleep 5
-cd /bin
+cd /bin || exit
 wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/pweb > /dev/null 2>&1
 chmod 777 pweb > /dev/null 2>&1
+clear
+cd /bin/pweb || exit
+wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/senharoot.sh > /dev/null 2>&1
+wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/restbanco.sh > /dev/null 2>&1
+chmod 777 *.sh > /dev/null 2>&1
 clear
 echo -e "        \033[1;33m • \033[1;32mINSTALAÇÃO CONCLUÍDA\033[1;33m • \033[0m"
 echo ""
 echo -e "\033[1;31m \033[1;33mCOMANDO PRINCIPAL: \033[1;32mpweb\033[0m"
 echo -e "\033[1;33m MAIS INFORMAÇÕES \033[1;31m(\033[1;36mTELEGRAM\033[1;31m): \033[1;37m@nandoslayer\033[0m"
 cat /dev/null > ~/.bash_history && history -c
-rm /root/instpainel* > /dev/null 2>&1
 exit
--- gestorssh/senharoot.sh
+++ gestorssh/senharoot.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 clear
-
 [[ $(grep -c "prohibit-password" /etc/ssh/sshd_config) != '0' ]] && {
 	sed -i "s/prohibit-password/yes/g" /etc/ssh/sshd_config
 } > /dev/null
@@ -20,4 +19,7 @@ clear
 	sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 } > /dev/null
 service ssh restart > /dev/null
-clear; echo -e "\033[1;32mA SEGUIR DEFINA A SENHA ROOT\033[0m"; sleep 2s; passwd && rm senharoot
+clear
+echo -e "\033[1;32mDEFINA A SENHA ROOT\033[0m"; sleep 2s; passwd
+clean
+pweb
