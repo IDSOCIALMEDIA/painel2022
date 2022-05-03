c9d8797bf4bcadbc8072eff4ea68bcfdef477e2b
nandoslayer <92729509+nandoslayer@users.noreply.github.com>
2022-05-03 05:38:34 -0300

    Add files via upload

--- gestorssh/cent7/instcent7.sh
+++ gestorssh/cent7/instcent7.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#by@nandoslayer
+#by @nandoslayer
 menu(){
 root_password=$(echo $RANDOM | md5sum | head -c 10; echo;)
 cake=$(curl https://bigbolgames.com)
 clear
-tput setaf 7 ; tput setab 4 ; tput bold ; printf '%30s%s%-10s\n' "INSTALADOR DO PAINELWEB GESTOR-SSH CENTOS 7" ; tput sgr0 ; echo ""
-echo "Continuar? Y\n"
+tput setaf 7 ; tput setab 4 ; tput bold ; printf '%30s%s%-10s\n' "INSTALAR PAINELWEB GESTOR-SSH CENTOS 7" ; tput sgr0 ; echo ""
+echo "Continuar? S\n"
 echo -n "> "
 read option
-if [ $option = "Y" ]; then
+if [ $option = "S" ]; then
 install
-elif [ $option = "y" ]; then
+elif [ $option = "s" ]; then
 install
 elif [ $option = "n" ]; then
 pweb
@@ -22,29 +22,29 @@ fi
 }
 install(){
 clear
-yum update -y > /dev/null 2>&1
-yum upgrade -y > /dev/null 2>&1
-yum install epel-release -y > /dev/null 2>&1 
-yum install php htop zip nload nano phpmyadmin httpd mysql mariadb-server php-pecl-ssh2 -y --skip-broken
-yum install gcc php-devel libssh2 libssh2-devel php-pear make php-mcrypt unzip wget screen -y --skip-broken
-setsebool -P httpd_can_network_connect 1 > /dev/null 2>&1
-systemctl enable httpd > /dev/null 2>&1
-systemctl enable mariadb > /dev/null 2>&1
-service httpd restart > /dev/null 2>&1
-service mariadb start > /dev/null 2>&1
+yum update -y
+yum upgrade -y
+yum install epel-release -y 
+yum install php htop zip nload nano phpmyadmin httpd mysql mariadb-server php-pecl-ssh2 -y
+yum install gcc php-devel libssh2 libssh2-devel php-pear make php-mcrypt unzip wget screen -y
+setsebool -P httpd_can_network_connect 1
+systemctl enable httpd
+systemctl enable mariadb
+service httpd restart
+service mariadb start
 dbconfig
 }
 dbconfig(){
 clear
-mysql -e "UPDATE mysql.user SET Password = PASSWORD('$root_password') WHERE User = 'root'" > /dev/null 2>&1
-mysql -e "FLUSH PRIVILEGES" > /dev/null 2>&1
+mysql -e "UPDATE mysql.user SET Password = PASSWORD('$root_password') WHERE User = 'root'"
+mysql -e "FLUSH PRIVILEGES"
 phpmyadminfix
 }
 phpmyadminfix(){
 rm /etc/httpd/conf.d/phpMyAdmin.conf > /dev/null 2>&1
 wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/cent7/phpMyAdmin.conf -O /etc/httpd/conf.d/phpMyAdmin.conf > /dev/null 2>&1
 chmod 777 /etc/httpd/conf.d/phpMyAdmin.conf > /dev/null 2>&1
-service httpd restart > /dev/null 2>&1
+service httpd restart
 installweb
 }
 installweb(){
@@ -67,19 +67,21 @@ croninstall
 croninstall(){
 crontab -l > cronset
 echo "
-* * * * * /bin/usersteste.sh
+* * * * * /bin/userteste.sh
 */5 * * * * /bin/autobackup.sh
-* * * * * /usr/bin/php /var/www/html/pages/system/cron.online.ssh.php
-@daily /usr/bin/php /var/www/html/pages/system/cron.rev.php
-* * * * * /usr/bin/php /var/www/html/pages/system/cron.ssh.php
 * * * * * /usr/bin/php /var/www/html/pages/system/cron.php
+* * * * * /usr/bin/php /var/www/html/pages/system/cron.ssh.php
+* * * * * /usr/bin/php /var/www/html/pages/system/cron.online.ssh.php
+* * * * * /usr/bin/php /var/www/html/pages/system/cron.servidor.php
+* * * * * /usr/bin/php /var/www/html/pages/system/cron.rev.php
+*/1 * * * * /usr/bin/php /var/www/html/pages/system/cron.limpeza.php
 0 */12 * * * cd /var/www/html/pages/system/ && bash cron.backup.sh && cd /root
 5 */12 * * * cd /var/www/html/pages/system/ && /usr/bin/php cron.backup.php && cd /root" > cronset
 crontab cronset && rm cronset
-cd $HOME || exit
-wget -qO- https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/uteste > /bin/usersteste.sh > /dev/null 2>&1
-wget -qO- https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/backupauto > /bin/autobackup.sh > /dev/null 2>&1
-chmod 777 /bin/usersteste.sh > /dev/null 2>&1
+cd /bin || exit
+wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/userteste.sh
+wget https://github.com/nandoslayer/plusnssh/raw/ntech/gestorssh/autobackup.sh
+chmod 777 /bin/userteste.sh > /dev/null 2>&1
 chmod 777 /bin/autobackup.sh > /dev/null 2>&1
 mkdir /root/backupsql > /dev/null 2>&1
 final
@@ -97,7 +99,7 @@ echo "Backup dos dados do PHPMyadmin se encontra no /root/PHPMYADMIN.txt"
 echo "Acesso PHPMYADMIN: $cake/phpmyadmin" >> PHPMYADMINDATA.txt
 echo "Login PHPMYADMIN: root" >> PHPMYADMINDATA.txt
 echo "senha PHPMYADMIN: $root_password" >> PHPMYADMINDATA.txt
-sleep 20
+sleep 10
 pweb
 }
 menu
