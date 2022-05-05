762c4bdb2ff301da6dcb3b3d6fd3ef02fb61133d
nandoslayer <92729509+nandoslayer@users.noreply.github.com>
2022-05-05 12:30:27 -0300

    Add files via upload

--- gestorssh/cent7/senharoot.sh
+++ gestorssh/cent7/senharoot.sh
@@ -19,13 +19,11 @@ clear
 	sed -i "s/#PasswordAuthentication no/PasswordAuthentication yes/g" /etc/ssh/sshd_config
 } > /dev/null
 clear
-echo -e "\033[1;32mDEFINA A SENHA ROOT\033[0m"; sleep 3s; passwd
-clean
-echo -e "        \033[1;33m • \033[1;32mSENHA ROOT ALTERADA COM SUCESSO\033[1;33m • \033[0m"
-echo ""
+echo -e "\033[1;32mDEFINA A SENHA ROOT\033[0m"; sleep 1s; passwd
+echo -e "\033[1;33m • \033[1;32mSENHA ROOT ALTERADA COM SUCESSO\033[1;33m • \033[0m"
 sleep 3s
-echo -e "        \033[1;33m • \033[1;32mSISTEMA RENICIANDO\033[1;33m • \033[0m"
+clear
+echo -e "\033[1;33m • \033[1;32mSISTEMA REINICIANDO\033[1;33m • \033[0m"
 echo ""
 sleep 3s
 shutdown -r now > /dev/null
-clear
